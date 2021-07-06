c3adb0c25193535c796efeb62ab918cec768873b
NoteEdu <eduardo.tsilva@hotmail.com>
2021-07-06 19:26:53 -0400

    alterando docker-compose correção no volume do rabbitMQ. Melhorando o RabbitMQ para criar a queue e exchange automaticamente e não mais manual

--- src/main/java/com/eduardo/crud/config/MessageConfig.java
+++ src/main/java/com/eduardo/crud/config/MessageConfig.java
@@ -1,7 +1,9 @@
 package com.eduardo.crud.config;
 
-import org.springframework.amqp.core.Exchange;
-import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,13 +13,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MessageConfig {
 
+	@Value("${crud.rabbitmq.routingkey}")
+	String routingKey;
+
 	@Value("${crud.rabbitmq.exchange}")
 	String exchange;
 
+	@Value("${crud.rabbitmq.queue}")
+	String queue;
+
 	@Bean
-	public Exchange declareExchange() {
-		return ExchangeBuilder.directExchange(exchange).durable(true).build();
+	Queue queue() {
+		return new Queue(queue, true);
 	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange(exchange);
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+	}
+
+//	@Bean
+//	public Exchange declareExchange() {
+//		return ExchangeBuilder.directExchange(exchange).durable(true).build();
+//	}
 
 	@Bean
 	public MessageConverter jsonMessageConverter() {
