04954f70d42b66b751e9d7248ea32ae62bd30abd
NoteEdu <eduardo.tsilva@hotmail.com>
2021-07-11 11:13:49 -0400

    Add eureka client e trocando versão do springboot

--- src/main/java/com/eduardo/crud/CrudApplication.java
+++ src/main/java/com/eduardo/crud/CrudApplication.java
@@ -2,9 +2,10 @@ package com.eduardo.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CrudApplication {
 	
 	public static void main(String[] args) {
